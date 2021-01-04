70d88efc2e763d352742248a18c854ed91be5082
ethanny2 <ethanny2@gmail.com>
2021-01-04 18:16:34 -0500

    Finished with initial setup for this boilerplate. Decided to stick with the CSS in JS solution sort of built into material-ui without using a theme unless I really feel like it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/styles';
+import Login from './Login';
+/*Returns JSS styling object;*/
+const useStyles = makeStyles({
+	root: {
+		margin: '0',
+		color: 'black',
+		fontFamily: 'Georgia, Times, serif',
+		boxSizing: 'border-box'
+	}
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const classes = useStyles();
+	return (
+		<main className={classes.root}>
+			{/* <h1>App</h1> */}
+      <Login></Login>
+		</main>
+	);
 }
 
 export default App;
