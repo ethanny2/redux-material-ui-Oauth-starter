9ea8b14c7464b269c47236d407e638c806a19115
ethanny2 <ethanny2@gmail.com>
2021-02-01 20:16:43 -0500

    Working on refactoring the shipped GoogleLogin component, no need for inputs for forms since that like open in a pop up window meaning I just have to make a pretty button

--- src/components/Login2.js
+++ src/components/Login2.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import {useGoogleLogin, useGoogleLogout} from 'react-google-login';
+import { useGoogleLogin, useGoogleLogout } from 'react-google-login';
+import googleLogo from '../images/google-logo.png';
+import { darken } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
 	paper: {
 		marginTop: theme.spacing(8),
 		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center'
+		alignItems: 'center',
+		// border: '1px solid red',
+		boxShadow: theme.shadows[3],
+		backgroundColor: theme.palette.primary.main,
+		color: theme.palette.primary.contrastText,
+		transition: 'background-color 0.5s',
+		'&:hover': {
+			backgroundColor: theme.palette.primary.dark,
+			transition: 'background-color 0.5s',
+			cursor: 'pointer'
+		}
 	},
 	avatar: {
-		margin: theme.spacing(1),
-		backgroundColor: theme.palette.secondary.main
+		margin: theme.spacing(1)
+	},
+	text: {
+		flexGrow: 1,
+		textAlign: 'center'
 	},
 	form: {
 		width: '100%', // Fix IE 11 issue.
@@ -38,64 +47,19 @@ export default function SignIn() {
 
 	return (
 		<Container component='section' maxWidth='xs'>
-			<div className={classes.paper}>
-				<Avatar className={classes.avatar}>
-					<LockOutlinedIcon />
-				</Avatar>
-				<Typography component='h1' variant='h5'>
-					Sign in
+			<Button className={classes.paper}>
+				<Avatar src={googleLogo} className={classes.avatar} />
+
+				<Typography component='h1' variant='h6' className={classes.text}>
+					Sign in with Google
 				</Typography>
-				<form className={classes.form} noValidate>
-					<TextField
-						variant='outlined'
-						margin='normal'
-						required
-						fullWidth
-						id='email'
-						label='Email Address'
-						name='email'
-						autoComplete='email'
-						autoFocus
-					/>
-					<TextField
-						variant='outlined'
-						margin='normal'
-						required
-						fullWidth
-						name='password'
-						label='Password'
-						type='password'
-						id='password'
-						autoComplete='current-password'
-					/>
-					<FormControlLabel
-						control={<Checkbox value='remember' color='primary' />}
-						label='Remember me'
-					/>
-					<Button
-						type='submit'
-						fullWidth
-						variant='contained'
-						color='primary'
-						className={classes.submit}
-					>
-						Sign In
-					</Button>
-					<Grid container>
-						<Grid item xs>
-							<Link href='#' variant='body2'>
-								Forgot password?
-							</Link>
-						</Grid>
-						<Grid item>
-							<Link href='#' variant='body2'>
-								{"Don't have an account? Sign Up"}
-							</Link>
-						</Grid>
-					</Grid>
-				</form>
-			</div>
-	
+			</Button>
+			{/* <div className={classes.paper}>
+				<Avatar src={googleLogo} className={classes.avatar}></Avatar>
+				<Typography component='h1' variant='h6' className={classes.text}>
+					Sign in with Google
+				</Typography>
+			</div> */}
 		</Container>
 	);
 }
