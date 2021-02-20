fb8345785474850be5913acef918d2380e130b28
ethanny2 <ethanny2@gmail.com>
2021-02-19 21:44:05 -0500

    Removed serverity enum that was not a prop for Snackbar component but rather for the Material UI Alert component which I don't use

--- src/actions/alertActions.js
+++ src/actions/alertActions.js
@@ -1,8 +1,7 @@
 import { SHOW_ALERT, CLEAR_ALERT } from '../types';
-import { ALERT_STATES } from '../reducers/alertReducer';
 
 export const showAlert = (
-	msgConfig = { message: 'default', severity: ALERT_STATES.info }
+	msgConfig = { message: 'default'}
 ) => ({ type: SHOW_ALERT, payload: { ...msgConfig } });
 
 export const clearAlert = () => ({ type: CLEAR_ALERT });
--- src/reducers/alertReducer.js
+++ src/reducers/alertReducer.js
@@ -1,19 +1,10 @@
 import { SHOW_ALERT, CLEAR_ALERT } from '../types';
 import Grow from '@material-ui/core/Grow';
 
-/* Enum for the possible values for severity */
-export const ALERT_STATES = {
-	error: 'error',
-	warning: 'warning',
-	info: 'info',
-	success: 'success'
-};
-Object.freeze(ALERT_STATES);
 
 /* Matches props of  <Snackbar /> component from Material UI*/
 const INITIAL_STATE = {
 	open: false,
-	severity: ALERT_STATES.info,
 	message: '',
 	anchorOrigin: { vertical: 'top', horizontal: 'center' },
 	TransitionComponent: Grow,
@@ -25,7 +16,7 @@ const alertReducer = (state = INITIAL_STATE, action) => {
 		case SHOW_ALERT:
 			/*
        action.payload looks like
-        {message:'', severity: ''}
+        {message:''}
       */
 			return { ...state, open: true, ...action.payload };
 		case CLEAR_ALERT:
